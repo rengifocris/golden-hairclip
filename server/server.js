f5382c5f86f87a88ca1c8158643bf7d8a5ebf6aa
JohanpG <johanpg@hotmail.com>
2019-07-10 02:26:46 -0500

    Added gcloud support

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
 const http = require('http');
+const https = require('https');
 const app = require('./app');
 const workers = require('./daily-quote-worker');
 
-const port = process.env.port || 3000;
+const port = process.env.port || 8080;
+const porthttps = process.env.port || 8081;
 
 const server = http.createServer(app);
+const serverHttps = https.createServer(app);
 // start the workers
 workers.init();
 
 server.listen(port);
-console.log(`Sever started at port --> ${port}`);
+serverHttps.listen(porthttps);
+console.log(`HTTP Sever started at port --> ${port}`);
+console.log(`HTTPS Sever started at port --> ${porthttps}`);
